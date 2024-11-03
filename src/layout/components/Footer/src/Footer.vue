01050459fb5624c3c22312aea10603a76536bc42
LIANGDELL\liang <liangwinlive@live.com>
2024-11-03 20:03:58 +0800

    fix:footer

--- src/layout/components/Footer/src/Footer.vue
+++ src/layout/components/Footer/src/Footer.vue
@@ -19,6 +19,7 @@ const title = computed(() => appStore.getTitle)
     :class="prefixCls"
     class="h-[var(--app-footer-height)] bg-[var(--app-content-bg-color)] text-center leading-[var(--app-footer-height)] text-[var(--el-text-color-placeholder)] dark:bg-[var(--el-bg-color)] overflow-hidden"
   >
-    <span class="text-14px">Copyright ©2022-{{ title }}</span>
+    <!--    <span class="text-14px">Copyright ©2022-{{ title }}</span> -->
+
   </div>
 </template>
